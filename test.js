7bd8e77073afee534d8fe6ad0d072dbe372fd495
bochaga-masaval <bochagavia@masaval.cl>
2018-01-19 10:32:56 -0300

    add decimal test

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import { numberToText } from './index'
 
 test('numberToText', t => {
   t.is(numberToText("0"), 'Cero')
+  t.is(numberToText("15,26"), 'Quince coma Veintiseis')
   t.is(numberToText("125400"), 'Ciento Veinticinco mil Cuatrocientos')
   t.is(numberToText("56894230"), 'Cincuenta y Seis Millones Ochocientos Noventa y Cuatro mil Doscientos Treinta')
   t.is(numberToText("12"), 'Doce')
